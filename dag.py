8cf1c9bf12d6258f1092958ee0ccdc0d292ae19a
marina <margo-2.8-9.2@yandex.ru>
2021-06-11 15:40:21 +0700

    Курсовая_

--- dag.py
+++ dag.py
@@ -114,6 +114,90 @@ with DAG(
             hub_names_list = hub_names.split('_')
             for a in hub_names_list:
                 hubs[a][source_table] >> task
+                
+    links = {
+        (l_hub_name, r_hub_name): {
+            table_name: DdsLOperator(
+                task_id='dds.l_{l_hub_name}_{r_hub_name}'.format(l_hub_name=l_hub_name, r_hub_name=r_hub_name),
+                pg_conn_str="host='db' port=5432 dbname='my_database' user='root' password='postgres'",
+                pg_meta_conn_str="host='db' port=5432 dbname='my_database' user='root' password='postgres'",
+                config=dict(
+                    l_hub_name=l_hub_name,
+                    r_hub_name=r_hub_name,
+                    l_bk_column=l_bk_column,
+                    r_bk_column=r_bk_column,
+                    source_table=table_name,
+                )
+            )
+            for table_name, cols in YAML_DATA['sources']['tables'].items()
+            for l_col in cols['columns']
+            for l_bk_column, inf in l_col.items()
+            if inf.get('bk_for') == l_hub_name
+            for r_col in cols['columns']
+            for r_bk_column, inf in r_col.items()
+            if inf.get('bk_for') == r_hub_name
+        }
+        for r_hub_name, info in YAML_DATA['groups']['hubs'].items()
+        for l_hub_name in info['links'].keys()
+    }
+
+    for (l_hub, r_hub), info in links.items():
+        for source_table, task in info.items():
+            hubs[l_hub][source_table] >> task
+            hubs[r_hub][source_table] >> task
+            
+            
+    satellites = {
+        
+            hub_name:{table:  DdsSOperator(
+                task_id='dds.s_{hub_name}'.format(hub_name=hub_name),
+                pg_conn_str="host='db' port=5432 dbname='my_database' user='root' password='postgres'",
+                pg_meta_conn_str="host='db' port=5432 dbname='my_database' user='root' password='postgres'",
+                config=dict(
+                    hub_name = hub_name,
+                    source_table = table,
+                    bk_column = bk_column
+                )
+            )
+            for table, cols in YAML_DATA['sources']['tables'].items()
+            for col in cols['columns']
+            for bk_column, inf in col.items()
+            if inf.get('bk_for') == hub_name and table == hub_name
+        }
+        for hub_name, info in YAML_DATA['groups']['hubs'].items()
+        for satellite_name in info['satellites'].keys()
+    }
+            
+    for hub, info in satellites.items():
+        for source_table, task in info.items():
+            hubs[hub][source_table] >> task
+            
+            
+    satellites_l = {
+        
+            link_name:{table:DdsSLOperator(
+                task_id='dds.l_s_{link_name}'.format(link_name=link_name),
+                pg_conn_str="host='db' port=5432 dbname='my_database' user='root' password='postgres'",
+                pg_meta_conn_str="host='db' port=5432 dbname='my_database' user='root' password='postgres'",
+                config=dict(
+                    link_name = link_name,
+                    source_table = table,
+                    bk_columns = [key for a in cols['columns'] for key, value in a.items() ]
+                )
+            )
+            
+            for table, cols in YAML_DATA['sources']['tables'].items()
+            if table == info['source_for_satellite']
+           
+            
+        }
+        for link_name, info in YAML_DATA['groups']['links'].items()
+        
+    }
+            
+    for link, info in satellites_l.items():
+        for source_table, task in info.items():
+            links_h[link][source_table] >> task
 
 
     
